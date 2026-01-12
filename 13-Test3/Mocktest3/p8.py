bab586658b3ba5e25b0ba4f94d9a83dab7c65e4d
Adrian <a.uchacz@op.pl>
2026-01-12 16:55:08 +0100

    change

--- 13-Test3/Mocktest3/p8.py
+++ 13-Test3/Mocktest3/p8.py
@@ -1,2 +1,9 @@
 def f(fnc, prods):
     return ','.join(fnc(prod) for prod in prods)
+
+if __name__ == "__main__":
+    prods = ["water","cheese","tomato"]
+    fnc1 = lambda x: "id:"+x[:2]
+    print(f(fnc1,prods))
+    fnc2 = lambda x: (x[0]+x[-1]).upper()
+    print(f(fnc2,prods))
